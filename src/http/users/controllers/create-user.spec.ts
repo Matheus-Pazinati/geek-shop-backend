7518d8731c03fa248d5c86e464562e8563445d1f
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-10-06 20:21:46 -0300

    create tests for create-user controller

--- src/http/users/controllers/create-user.spec.ts
+++ src/http/users/controllers/create-user.spec.ts
@@ -23,4 +23,37 @@ describe("Create user E2E test", () => {
     })
     .expect(201)
   })
+
+  test("it should not be able to create users with same e-mail", async() => {
+    await request(app)
+    .post('/users')
+    .send({
+      name: "Pedro",
+      email: "pedro@example.com",
+      password: "12345678",
+      passwordConfirm: "12345678"
+    })
+
+    await request(app)
+    .post('/users')
+    .send({
+      name: "Pedro",
+      email: "pedro@example.com",
+      password: "12345678",
+      passwordConfirm: "12345678"
+    })
+    .expect(409)
+  })
+
+  test("it should not be able to create user with wrong password confirm", async() => {
+    await request(app)
+    .post('/users')
+    .send({
+      name: "Pedro",
+      email: "pedro@email.com",
+      password: "12345678",
+      passwordConfirm: "123456789"
+    })
+    .expect(400)
+  })
 })
