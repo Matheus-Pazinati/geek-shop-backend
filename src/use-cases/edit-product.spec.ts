6754162515ad0cc138ca05de51e7532703bda31d
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-10-23 22:28:49 -0300

    change the edit-product use-case for check if the owner of the product is the same that is trying to edit the product

--- src/use-cases/edit-product.spec.ts
+++ src/use-cases/edit-product.spec.ts
@@ -3,6 +3,7 @@ import { beforeEach, describe, expect, test } from 'vitest'
 import { EditProductUseCase } from './edit-product'
 import { makeProduct } from 'test/factories/make-product'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 describe('Edit a Product Test', () => {
   let inMemoryProductsRepository: InMemoryProductsRepository
@@ -27,6 +28,7 @@ describe('Edit a Product Test', () => {
         name: 'new-name',
         price: 10,
       },
+      ownerId: product.ownerId
     })
 
     expect(inMemoryProductsRepository.products[0]).toEqual(
@@ -35,7 +37,7 @@ describe('Edit a Product Test', () => {
   })
 
   test('it should not be able to edit a nonexistent product', async () => {
-    expect(async () => {
+    expect(async() => {
       await editProduct.execute({
         newProductData: {
           id: 'nonexistent-id',
@@ -44,8 +46,31 @@ describe('Edit a Product Test', () => {
           category: 'generics',
           description: 'fake-description',
           imageUrl: 'fake-url',
+          ownerId: '1aea40ac-6f8b-11ee-b962-0242ac120002'
         },
+        ownerId: '1aea40ac-6f8b-11ee-b962-0242ac120002'
       })
     }).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  test('it should not be able to edit a product from another user', async() => {
+    const productOwnerId = "1fdd3ea6-7207-11ee-b962-0242ac120002"
+    const product = await makeProduct({
+      ownerId: productOwnerId
+    })
+
+    inMemoryProductsRepository.create(product)
+
+    const anotherOwnerId = "b8f35292-7207-11ee-b962-0242ac120002"
+
+    expect(async() => {
+      await editProduct.execute({
+        newProductData: {
+          ...product,
+          name: 'new-product'
+        },
+        ownerId: anotherOwnerId
+      })
+    }).rejects.toBeInstanceOf(NotAllowedError)
   })
 })
--- src/use-cases/edit-product.ts
+++ src/use-cases/edit-product.ts
@@ -1,19 +1,27 @@
 import { Product } from '@/database/models/product'
 import { ProductsRepository } from '@/database/repositories/products-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 interface EditProductUseCaseRequest {
   newProductData: Product
+  ownerId: string
 }
 
 export class EditProductUseCase {
   constructor(private productsRepository: ProductsRepository) {}
 
-  async execute({ newProductData }: EditProductUseCaseRequest) {
+  async execute({ newProductData, ownerId }: EditProductUseCaseRequest) {
     const product = await this.productsRepository.findById(newProductData.id!)
 
     if (!product) {
       throw new ResourceNotFoundError()
+    }
+
+    const isOwner = await this.productsRepository.verifyProductOwner(newProductData.id!, ownerId)
+
+    if (!isOwner) {
+      throw new NotAllowedError()
     }
 
     await this.productsRepository.save(newProductData)
