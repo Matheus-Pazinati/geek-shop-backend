a9b0eb4f8e653dd372f440c99cb993822d98b998
Matheus Martins Pazinati <matheuspazinati@gmail.com>
2023-11-01 16:19:35 -0300

    add the cors module

--- app.ts
+++ app.ts
@@ -1,13 +1,16 @@
 import express from "express"
 import { usersRouter } from 'src/http/users/routes'
 import { productsRouter } from "src/http/products/routes"
+import cors from "cors"
 
-export const app = express()
+export const app = express();
 
 app.use(express.static('public'));
 
-app.use(express.json())
+app.use(cors());
 
-app.use("/", usersRouter)
+app.use(express.json());
 
-app.use("/products", productsRouter)
+app.use("/", usersRouter);
+
+app.use("/products", productsRouter);
